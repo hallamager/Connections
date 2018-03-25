4203c6055e7e3ad8b9da0283f83026bce1165128
hallamager <HallamAger@Hallams-MacBook-Pro.local>
2018-03-25 15:25:26 +0100

    showing count of application with relevant business job

--- Connections/StudentJobs/Posted/JobsPostedViewController.swift
+++ Connections/StudentJobs/Posted/JobsPostedViewController.swift
@@ -22,6 +22,7 @@ class JobsPostedViewController: UIViewController {
     var students = [Student]()
     var business: Business!
     var job: Job!
+    var student: Student!
     let animations = [AnimationType.from(direction: .bottom, offset: 30.0)]
     
     override func viewDidLoad() {
@@ -44,6 +45,14 @@ class JobsPostedViewController: UIViewController {
             print("is\(self.jobs.count)")
             
         })
+        
+        //open menu with tab bar button
+        openMenu.target = self.revealViewController()
+        openMenu.action = #selector(SWRevealViewController.revealToggle(_:))
+        
+        
+        //open menu with swipe gesture
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         
     }
     
@@ -79,6 +88,44 @@ extension JobsPostedViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "jobsPostedCell") as! JobsPostedCell
         let job = jobs[indexPath.row]
+        
+        
+        func loadRelatedStudents(for businessUID: String, completion: @escaping (Bool, [Student]) -> ()) {
+            
+            let ref = Database.database().reference(withPath: "jobsApplied/" + Auth.auth().currentUser!.uid).child(job.uuid!)
+            ref.observeSingleEvent(of: .value) { snapshot in
+                
+                var uids = [String]()
+                for child in snapshot.children {
+                    let userData = child as! DataSnapshot
+                    uids.append(userData.key)
+                }
+                
+                let userRef = Database.database().reference(withPath: "student")
+                var students = [Student]()
+                var count = 0
+                if uids.count != 0 {
+                    uids.forEach { uid in
+                        userRef.child(uid).observeSingleEvent(of: .value) { snapshot in
+                            let student = Student(snapshot: snapshot)
+                            students.append(student!)
+                            count += 1
+                            if count == uids.count {
+                                completion(true, students)
+                            }
+                        }
+                    }
+                } else {
+                    completion(true, students)
+                }
+            }
+        }
+        
+        loadRelatedStudents(for: Auth.auth().currentUser!.uid) { success, students in
+            self.students = students
+            print("hi\(students.count)")
+            cell.applicantCount?.text = String(students.count)
+        }
         
         cell.jobTitle?.text = job.title
         cell.employmentType?.text = job.employmentType
