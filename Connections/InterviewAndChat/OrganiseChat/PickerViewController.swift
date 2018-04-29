b9770d1ffa962861b14d2756296f17836669c42e
hallamager <HallamAger@Hallams-MacBook-Pro.local>
2018-04-29 15:27:50 +0100

    Organise Interview UI Update

---
+++ Connections/InterviewAndChat/OrganiseChat/PickerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PickerViewController.swift
+//  Connections
+//
+//  Created by Hallam John Ager on 28/04/2018.
+//  Copyright © 2018 Hallam John Ager. All rights reserved.
+//
+
+import Foundation
+
+class PickerViewController: UIViewController {
+    
+    
+    @IBAction func closeModalBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
